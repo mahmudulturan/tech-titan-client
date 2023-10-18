02872fd0040423321fc3b5a39b8d5077c4b6d434
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-18 20:18:29 +0600

    Add: Footer Section

--- src/RootElement.jsx
+++ src/RootElement.jsx
@@ -2,12 +2,14 @@ import Header from "./components/Header/Header";
 import {Outlet} from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Footer from "./components/Footer/Footer";
 AOS.init();
 const RootElement = () => {
     return (
-        <div className="bg-white dark:bg-gray-900 dark:text-white text-text transition duration-300">
+        <div className="bg-white dark:bg-gray-900 dark:text-white text-text transition duration-300 font-ubuntu">
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="font-ubuntu">
             <Banner></Banner>
-            <h1 className="min-h-screen"></h1>
+            {/* <h1 className="min-h-screen"></h1> */}
         </div>
     );
 };
