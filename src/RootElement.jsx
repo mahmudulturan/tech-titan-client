86ddb83171608cbf1197008deb74d6ff7136643f
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2023-10-18 23:50:06 +0600

    Add: Product adding page and send data to the server

--- src/RootElement.jsx
+++ src/RootElement.jsx
@@ -1,17 +1,20 @@
 import Header from "./components/Header/Header";
-import {Outlet} from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { Outlet } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Footer from "./components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
+
 AOS.init();
 const RootElement = () => {
-    return (
-        <div className="bg-white dark:bg-gray-900 dark:text-white text-text transition duration-300 font-ubuntu">
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white text-text transition duration-300 font-ubuntu">
+      <Toaster position="top-center" reverseOrder={false} />
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default RootElement;
--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,9 +1,151 @@
+import axios from "axios";
+import toast from 'react-hot-toast';
+
+
 const AddProduct = () => {
-    return (
-        <div>
-            Add Product
+  const handelAddProduct = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const brand = form.brand.value;
+    const type = form.type.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const image = form.photo.value;
+    const detail = form.detail.value;
+    const productData = {
+      name,
+      brand,
+      type,
+      price,
+      rating,
+      image,
+      detail,
+    };
+    axios.post('http://localhost:5000/products',productData, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res=>{
+        const data = res.data;
+        if(data.acknowledged){
+            toast.success('Successfully Added!')
+            form.reset()
+        }
+    })
+    .catch(error=> {
+        toast.error(error.message)
+    })
+  };
+  return (
+    <div className="min-h-[93vh] my-10">
+      <div className="md:w-8/12 mx-auto bg-background dark:bg-text py-8 md:px-10 px-2 rounded-md shadow-lg">
+        <div className="text-center">
+          <h3 className="font-bold text-3xl mb-4 drop-shadow-md">
+            Add New Product
+          </h3>
+          <p className="max-w-3xl mx-auto font-medium">
+            Introduce your latest tech marvel to our collection. Fill out the
+            form below to add a new product to our offerings.
+          </p>
         </div>
-    );
+        <form
+          onSubmit={handelAddProduct}
+          className="grid grid-cols-1 md:grid-cols-2 gap-4 my-4">
+          <div>
+            <label htmlFor="name" className="block font-medium my-2 text-lg">
+              Name:
+            </label>
+            <input
+              placeholder="Product Name..."
+              type="text"
+              name="name"
+              id="name"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="brand" className="block font-medium my-2 text-lg">
+              Brand Name:
+            </label>
+            <input
+              placeholder="Brand Name..."
+              type="text"
+              name="brand"
+              id="brand"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="type" className="block font-medium my-2 text-lg">
+              Type:
+            </label>
+            <input
+              placeholder="Product Type..."
+              type="text"
+              name="type"
+              id="type"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="price" className="block font-medium my-2 text-lg">
+              Price:
+            </label>
+            <input
+              placeholder="Product Price..."
+              type="text"
+              name="price"
+              id="price"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="rating" className="block font-medium my-2 text-lg">
+              Rating:
+            </label>
+            <input
+              placeholder="Product Rating..."
+              type="text"
+              name="rating"
+              id="rating"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div>
+            <label htmlFor="photo" className="block font-medium my-2 text-lg">
+              Image:
+            </label>
+            <input
+              placeholder="Product Image URL..."
+              type="text"
+              name="photo"
+              id="photo"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <div className="md:col-span-2">
+            <label htmlFor="detail" className="block font-medium my-2 text-lg">
+              Short description:
+            </label>
+            <input
+              placeholder="Product Short description..."
+              type="text"
+              name="detail"
+              id="detail"
+              className="py-2 px-3 rounded-md w-full outline-none dark:bg-slate-900 dark:placeholder:text-white"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Add Product"
+            className="py-2 px-3 rounded-md bg-primary md:col-span-2 font-medium text-white cursor-pointer"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
